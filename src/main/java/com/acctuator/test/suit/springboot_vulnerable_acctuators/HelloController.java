d14390a45627b8018d15de7cb07d5928b17c0c7d
nikhl1232 <nikhilsahoo1232@gmail.com>
2024-04-01 20:46:08 +0530

    [V2]

--- src/main/java/com/acctuator/test/suit/springboot_vulnerable_acctuators/HelloController.java
+++ src/main/java/com/acctuator/test/suit/springboot_vulnerable_acctuators/HelloController.java
@@ -3,11 +3,18 @@ package com.acctuator.test.suit.springboot_vulnerable_acctuators;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 public class HelloController {
 
+    private static final Logger LOGGER = 
+    LoggerFactory.getLogger(HelloController.class);
+
     @RequestMapping("/")
     public String index() {
+        LOGGER.debug("Hitting the root /");
         return "Greetings from Spring Boot!";
     }
 }
--- src/main/java/com/acctuator/test/suit/springboot_vulnerable_acctuators/SpringbootVulnerableAcctuatorsApplication.java
+++ src/main/java/com/acctuator/test/suit/springboot_vulnerable_acctuators/SpringbootVulnerableAcctuatorsApplication.java
@@ -1,12 +1,32 @@
 package com.acctuator.test.suit.springboot_vulnerable_acctuators;
 
+import java.lang.management.ManagementFactory;
+
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+
+import java.lang.management.*; 
+import javax.management.*; 
+
+import org.hibernate.criterion.Example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class SpringbootVulnerableAcctuatorsApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
+
+    // String objectName = "com.acctuator.test.suit.springboot_vulnerable_acctuators.mbean:name=Ping";
+
+
+	// 	MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+	// 	Ping mbean = new Ping();
+	// 	ObjectName objname = new ObjectName(objectName);
+
+	// 	mbs.registerMBean(mbean, objname);
+
 		SpringApplication.run(SpringbootVulnerableAcctuatorsApplication.class, args);
 	}
 
